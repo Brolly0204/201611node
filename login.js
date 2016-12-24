b92326ba4f6be66e01d91d4d8225b27b6817750e
liwenli <102495553@qq.com>
2016-12-24 20:20:44 +0800

    logins

--- login.js
+++ login.js
@@ -6,20 +6,20 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
 app.set('view engine', 'ejs');
-app.set('views',path.resolve('../login'));
+app.set('views', path.resolve('views'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 var list = [];
 app.get('/', function (req, res) {
     //res.sendFile(path.resolve('assign.html'));
-    res.render('assign',{title:'注册页'})
+    res.render('assign', {title: '注册页'})
 });
 app.post('/assign', function (req, res) {
     list.push(req.body);
     res.redirect('/login');
 });
 app.get('/login', function (req, res) {
-    res.render('login',{title:'登录页'});
+    res.render('login', {title: '登录页'});
     //res.sendFile(path.resolve('login.html'));
 });
 app.post('/loginup', function (req, res) {
@@ -28,7 +28,7 @@ app.post('/loginup', function (req, res) {
     });
     if (flag) {
         //res.sendFile(path.resolve('ind.html'));
-        res.render('ind',{title:'欢迎页！'});
+        res.render('welcome', {title: '欢迎页！'});
     } else {
         res.redirect('/login');
     }
